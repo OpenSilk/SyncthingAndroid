Zillode/syncthing-silk

Set local Syncthing port to 8385

app/src/main/java/syncthing/android/service/SyncthingThread.java
@@ -58,6 +58,7 @@ public class SyncthingThread extends Thread {
                 Timber.e("Failed to generate config");
                 return;
             }
+            configXml.changeDefaultAddress();
             configXml.changeDefaultFolder();
         }
         configXml.updateIfNeeded();

OpenSilk/SyncthingAndroid

app/src/main/java/syncthing/android/service/SyncthingThread.java
@@ -58,6 +58,7 @@ public class SyncthingThread extends Thread {
                 Timber.e("Failed to generate config");
                 return;
             }
+            configXml.changeDefaultGUIAddress();
             configXml.changeDefaultFolder();
         }
         configXml.updateIfNeeded();