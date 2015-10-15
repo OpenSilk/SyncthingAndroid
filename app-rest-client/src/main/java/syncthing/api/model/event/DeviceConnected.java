653099d406a1a5c59399da07b574512a7eaf1d1a
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-10-15 15:32:40 -0500

    add client info to deviceconnected data
    Change-Id: Iac226ff3a61562faee3ac99c4b4f924fafaa1bdd

--- app-rest-client/src/main/java/syncthing/api/model/event/DeviceConnected.java
+++ app-rest-client/src/main/java/syncthing/api/model/event/DeviceConnected.java
@@ -30,5 +30,7 @@ public class DeviceConnected extends Event<DeviceConnected.Data> {
     public static class Data {
         public String addr;
         public String id;
+        public String clientName;
+        public String clientVersion;
     }
 }
