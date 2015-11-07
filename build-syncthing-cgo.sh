b3ec1e1ee36c1d7f1fe38fa70d595a0d50f0fdb3
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-11-07 16:26:31 -0600

    remove redundant build scripts
    Change-Id: I9cbc1ff580486a41470234a29ba35c8748964648

--- build-syncthing-cgo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-# Make a different copy for x86 and ARM (See https://github.com/golang/go/issues/8161 and https://github.com/golang/go/issues/9114)
-unset GOROOT
-./build-syncthing-cgo-arm.sh
-./build-syncthing-cgo-x86.sh
