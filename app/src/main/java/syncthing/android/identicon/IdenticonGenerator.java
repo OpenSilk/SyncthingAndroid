bec77c201b8b9909eecc1840a3421e7894cd1e4b
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-11-08 21:52:20 -0600

    make identicons grey, unifify files
    Change-Id: I2035df510eda8ff6f8bd8db7c52f6b19a029e210

--- app/src/main/java/syncthing/android/identicon/IdenticonGenerator.java
+++ app/src/main/java/syncthing/android/identicon/IdenticonGenerator.java
@@ -16,8 +16,8 @@ import rx.schedulers.Schedulers;
 //https://github.com/davidhampgonsalves/Contact-Identicons
 @Singleton
 public class IdenticonGenerator {
-	public static int height = 5;
-	public static int width = 5;
+    public static int height = 5;
+    public static int width = 5;
 
     final HashGeneratorInterface hashGenerator;
 
@@ -43,42 +43,37 @@ public class IdenticonGenerator {
     }
 
     public Bitmap generate(String userName) {
-		
-		byte[] hash = hashGenerator.generate(userName);
 
-		Bitmap identicon = Bitmap.createBitmap(width, height, Config.ARGB_8888);
+        byte[] hash = hashGenerator.generate(userName);
 
-		// get byte values as unsigned ints
-		int r = hash[0] & 255;
-		int g = hash[1] & 255;
-		int b = hash[2] & 255;
+        Bitmap identicon = Bitmap.createBitmap(width, height, Config.ARGB_8888);
 
-		int background = Color.TRANSPARENT;
-		int foreground = Color.argb(255, r, g, b);
+        int background = Color.TRANSPARENT;
+        int foreground = Color.parseColor("#757575");
 
-		for (int x = 0; x < width; x++) {
-			
-			//make identicon horizontally symmetrical 
-			int i = x < 3 ? x : 4 - x;
-			int pixelColor;
-			for (int y = 0; y < height; y++) {
-				
-				if ((hash[i] >> y & 1) == 1)
-					pixelColor = foreground;
-				else
-					pixelColor = background;
+        for (int x = 0; x < width; x++) {
 
-				identicon.setPixel(x, y, pixelColor);
-			}
-		}
-		
-		//scale image by 2 to add border
-		Bitmap bmpWithBorder = Bitmap.createBitmap(12, 12, identicon.getConfig());
-	    Canvas canvas = new Canvas(bmpWithBorder);
-	    canvas.drawColor(background);
-	    identicon = Bitmap.createScaledBitmap(identicon, 10, 10, false);
-	    canvas.drawBitmap(identicon, 1, 1, null);
-	    
-		return bmpWithBorder;
-	}
+            //make identicon horizontally symmetrical
+            int i = x < 3 ? x : 4 - x;
+            int pixelColor;
+            for (int y = 0; y < height; y++) {
+
+                if ((hash[i] >> y & 1) == 1)
+                    pixelColor = foreground;
+                else
+                    pixelColor = background;
+
+                identicon.setPixel(x, y, pixelColor);
+            }
+        }
+
+        //scale image by 2 to add border
+        Bitmap bmpWithBorder = Bitmap.createBitmap(12, 12, identicon.getConfig());
+        Canvas canvas = new Canvas(bmpWithBorder);
+        canvas.drawColor(background);
+        identicon = Bitmap.createScaledBitmap(identicon, 10, 10, false);
+        canvas.drawBitmap(identicon, 1, 1, null);
+
+        return bmpWithBorder;
+    }
 }
--- app/src/main/java/syncthing/android/identicon/MessageDigestHashGenerator.java
+++ app/src/main/java/syncthing/android/identicon/MessageDigestHashGenerator.java
@@ -2,22 +2,28 @@ package syncthing.android.identicon;
 
 import java.security.MessageDigest;
 
+import timber.log.Timber;
+
 //https://github.com/davidhampgonsalves/Contact-Identicons
 public class MessageDigestHashGenerator implements HashGeneratorInterface {
-	MessageDigest messageDigest;
+    MessageDigest messageDigest;
 
-	public MessageDigestHashGenerator(String algorithim) {
-		try {
-			messageDigest = MessageDigest.getInstance(algorithim);
-		}catch(Exception e) {
-			System.err.println("Error setting algorithim: " + algorithim);
-		}
-	}
-
-	public byte[] generate(String input) {
-        synchronized (this) {
-            return messageDigest.digest(input.getBytes());
+    public MessageDigestHashGenerator(String algorithim) {
+        try {
+            messageDigest = MessageDigest.getInstance(algorithim);
+        }catch(Exception e) {
+            Timber.e(e, "Unable to obtain digest %s", algorithim);
         }
-	}
+    }
 
+    public byte[] generate(String input) {
+        synchronized (this) {
+            if (messageDigest != null) {
+                messageDigest.reset();
+                return messageDigest.digest(input.getBytes());
+            } else {
+                return input.getBytes();
+            }
+        }
+    }
 }
